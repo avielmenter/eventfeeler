b0a4bb6bd4d8a71755f9bc458d5251ef611f80cb
avielmenter <aviel.menter@gmail.com>
2018-02-11 00:58:02 -0800

    server handles 404 errors

--- app.js
+++ app.js
@@ -34,20 +34,24 @@ app.use('/api', api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+    var err = new Error('Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+
+    if (err.status == 404)
+        res.sendFile(__dirname + '/public/404.html');
+    else
+        res.render('error');
 });
 
 module.exports = app;

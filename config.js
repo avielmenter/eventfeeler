c597bb5baa24bafc362ba5c160cd222325394b6d
avielmenter <aviel.menter@gmail.com>
2018-02-11 23:04:01 -0800

    config now relies on environment variables

---
+++ config.js
@@ -0,0 +1,17 @@
+var config = {};
+
+config.port = process.env.EVENTFEELER_PORT;
+
+config.dbServer = process.env.EVENTFEELER_DB_SERVER;
+config.dbSchema = process.env.EVENTFEELER_DB_SCHEMA;
+config.dbUser = process.env.EVENTFEELER_DB_USER;
+config.dbPassword = process.env.EVENTFEELER_DB_PASSWORD;
+config.dbOptions = process.env.EVENTFEELER_DB_OPTIONS;
+
+config.FB_APP_ID = process.env.EVENTFEELER_FB_APP_ID;
+config.FB_APP_SECRET = process.env.EVENTFEELER_FB_APP_SECRET;
+
+config.dbAuth = config.dbUser === undefined ? "" : (config.dbUser + ":" + config.dbPassword + "@");
+config.mongo = "mongodb://" + config.dbAuth + config.dbServer + "/" + config.dbSchema + "?" + (config.dbOptions === undefined ? "" : config.dbOptions);
+
+module.exports = config;

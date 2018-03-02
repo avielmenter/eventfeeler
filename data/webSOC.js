9b1558d8144f74aef61c0ad8c3ca5563e44588c3
avielmenter <aviel.menter@gmail.com>
2018-03-01 16:19:47 -0800

    more progress on WebSOC scraping

--- data/webSOC.js
+++ data/webSOC.js
@@ -3,7 +3,17 @@ const cheerio = require('cheerio');
 var querystring = require('querystring');
 var stringify = require('json-stringify-safe');
 
-var DEPARTMENTS = ['AC ENG', 'AFAM', 'ANATOMY', 'ANESTH', 'ANTHRO', 'ARABIC', 'ART', 'ART HIS', 'ART STU', 'ARTS', 'ARTSHUM', 'ASIANAM', 'BANA', 'BATS', 'BIO SCI', 'BIOCHEM', 'BME', 'BSEMD', 'CAMPREC', 'CBEMS', 'CEM', 'CHC/LAT', 'CHEM', 'CHINESE', 'CLASSIC', 'CLT&amp;THY', 'COGS', 'COM LIT', 'COMPSCI', 'CRITISM', 'CRM/LAW', 'CSE', 'DANCE', 'DERM', 'DEV BIO', 'DRAMA', 'E ASIAN', 'EARTHSS', 'ECO EVO', 'ECON', 'ECPS', 'ED AFF', 'EDUC', 'EECS', 'EHS', 'ENGLISH', 'ENGR', 'ENGRCEE', 'ENGRMAE', 'ENGRMSE', 'EPIDEM', 'ER MED', 'EURO ST', 'FAM MED', 'FIN', 'FLM&amp;MDA', 'FRENCH', 'GEN&amp;SEX', 'GERMAN', 'GLBL ME', 'GLBLCLT', 'GREEK', 'HEBREW', 'HINDI', 'HISTORY', 'HUMAN', 'HUMARTS', 'I&amp;C SCI', 'IN4MATX', 'INT MED', 'INTL ST', 'ITALIAN', 'JAPANSE', 'KOREAN', 'LATIN', 'LAW', 'LINGUIS', 'LIT JRN', 'LPS', 'M&amp;MG', 'MATH', 'MED', 'MED ED', 'MED HUM', 'MGMT', 'MGMT EP', 'MGMT FE', 'MGMT HC', 'MGMTMBA', 'MGMTPHD', 'MIC BIO', 'MOL BIO', 'MPAC', 'MUSIC', 'NET SYS', 'NEURBIO', 'NEUROL', 'NUR SCI', 'OB/GYN', 'OPHTHAL', 'PATH', 'PED GEN', 'PEDS', 'PERSIAN', 'PHARM', 'PHILOS', 'PHRMSCI', 'PHY SCI', 'PHYSICS', 'PHYSIO', 'PLASTIC', 'PM&amp;R', 'POL SCI', 'PORTUG', 'PP&amp;D', 'PSY BEH', 'PSYCH', 'PUB POL', 'PUBHLTH', 'RADIO', 'REL STD', 'ROTC', 'RUSSIAN', 'SOC SCI', 'SOCECOL', 'SOCIOL', 'SPANISH', 'SPPS', 'STATS', 'SURGERY', 'TAGALOG', 'TOX', 'UCDC', 'UNI AFF', 'UNI STU', 'VIETMSE', 'VIS STD', 'WOMN ST', 'WRITING'];
+var TERMS = ['2018-03', '2018-14'];
+
+var DEPARTMENTS = ['AC ENG']//, 'AFAM', 'ANATOMY', 'ANESTH', 'ANTHRO', 'ARABIC', 'ART', 'ART HIS', 'ART STU', 'ARTS', 'ARTSHUM', 'ASIANAM', 'BANA', 'BATS', 'BIO SCI', 'BIOCHEM', 'BME', 'BSEMD', 'CAMPREC', 'CBEMS', 'CEM', 'CHC/LAT', 'CHEM', 'CHINESE', 'CLASSIC', 'CLT&amp;THY', 'COGS', 'COM LIT', 'COMPSCI', 'CRITISM', 'CRM/LAW', 'CSE', 'DANCE', 'DERM', 'DEV BIO', 'DRAMA', 'E ASIAN', 'EARTHSS', 'ECO EVO', 'ECON', 'ECPS', 'ED AFF', 'EDUC', 'EECS', 'EHS', 'ENGLISH', 'ENGR', 'ENGRCEE', 'ENGRMAE', 'ENGRMSE', 'EPIDEM', 'ER MED', 'EURO ST', 'FAM MED', 'FIN', 'FLM&amp;MDA', 'FRENCH', 'GEN&amp;SEX', 'GERMAN', 'GLBL ME', 'GLBLCLT', 'GREEK', 'HEBREW', 'HINDI', 'HISTORY', 'HUMAN', 'HUMARTS', 'I&amp;C SCI', 'IN4MATX', 'INT MED', 'INTL ST', 'ITALIAN', 'JAPANSE', 'KOREAN', 'LATIN', 'LAW', 'LINGUIS', 'LIT JRN', 'LPS', 'M&amp;MG', 'MATH', 'MED', 'MED ED', 'MED HUM', 'MGMT', 'MGMT EP', 'MGMT FE', 'MGMT HC', 'MGMTMBA', 'MGMTPHD', 'MIC BIO', 'MOL BIO', 'MPAC', 'MUSIC', 'NET SYS', 'NEURBIO', 'NEUROL', 'NUR SCI', 'OB/GYN', 'OPHTHAL', 'PATH', 'PED GEN', 'PEDS', 'PERSIAN', 'PHARM', 'PHILOS', 'PHRMSCI', 'PHY SCI', 'PHYSICS', 'PHYSIO', 'PLASTIC', 'PM&amp;R', 'POL SCI', 'PORTUG', 'PP&amp;D', 'PSY BEH', 'PSYCH', 'PUB POL', 'PUBHLTH', 'RADIO', 'REL STD', 'ROTC', 'RUSSIAN', 'SOC SCI', 'SOCECOL', 'SOCIOL', 'SPANISH', 'SPPS', 'STATS', 'SURGERY', 'TAGALOG', 'TOX', 'UCDC', 'UNI AFF', 'UNI STU', 'VIETMSE', 'VIS STD', 'WOMN ST', 'WRITING'];
+
+function stripSpaces(s) {
+    return s.replace('\n', ' ')
+            .replace('\r', ' ')
+            .replace('\t', ' ')
+            .replace(/\s+/g, ' ')
+            .trim();
+}
 
 class webSOC {
     constructor (setAPI) {
@@ -13,47 +23,52 @@ class webSOC {
     getClassesInDepartment(html) {
         var $ = cheerio.load(html);
         var classes = []
-        $('.CourseTitle').each((i, elem) => {
-            var c = {};
+        $('.CourseTitle').each((i, elem) => {   // for each course
+            var className = stripSpaces($(elem).text().replace('(Prerequisites)', ''));
+            var table = $(elem).parent().parent();
 
-            c.name = $(elem).text()
-                            .replace('(Prerequisites)', '')
-                            .replace('\n\t', '')
-                            .replace(/\s+/g, ' ')
-                            .trim();
+            $('tr:not([class!=""])', table).each((j, tr) => {   // for each lecture for the course
+                var isLecture = false;
+                var c = { name: className };
 
-            var tr = $(elem).parent();
-            var lecTR = $(tr).next().next();
+                $('td', tr).each((i, td) => {
+                    if (i == 1)
+                        if (!$(td).text().match(/\s*Lec\s*/i))
+                            return false;
+                        else
+                            isLecture = true;
 
-            $('td', lecTR).each((i, elem) => {
-                if (i != 5)
-                    return true;
+                    switch (i) {
+                        case 2: c.sec       = stripSpaces($(td).text()); break;
+                        case 5: c.time      = stripSpaces($(td).text()); break;
+                        case 6: c.place     = stripSpaces($(td).text()); break;
+                        case 7: c.final     = stripSpaces($(td).text()); break;
+                        case 9: c.enrolled  = stripSpaces($(td).text()); break;
+                    }
+                });
 
-                var place = $(elem).next();
-                var final = $(place).next();
-                var enrolled = $(final).next().next();
-
-                c.time = $(elem).text();
-                c.place = $(place).text();
-                c.final = $(final).text();
-                c.enrolled = $(enrolled).text();
-
-                return false;
-            });
-
-            classes[i] = c;
+                if (isLecture)
+                    classes.push(c);
+            })
         });
 
         return classes;
     }
 
     async get() {
-        var requests = DEPARTMENTS.map(d =>
-            axios.post('https://www.reg.uci.edu/perl/WebSoc', querystring.stringify({
-                YearTerm: '2018-14',
-                Dept: d
-            }))
-        );
+        var requests = [];
+
+        for (let t of TERMS) {
+            var termReqs = DEPARTMENTS.map(d =>
+                axios.post('https://www.reg.uci.edu/perl/WebSoc', querystring.stringify({
+                    YearTerm: t,
+                    ShowFinals: true,
+                    Dept: d
+                }))
+            );
+
+            requests = requests.concat(termReqs);
+        }
 
         var responses = await Promise.all(requests);
 

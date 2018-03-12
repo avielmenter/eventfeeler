d597790fa310c2f49839e05d08c706b64fd66670
avielmenter <aviel.menter@gmail.com>
2018-03-12 14:29:40 -0700

    search now submits when you hit enter

--- public/search.js
+++ public/search.js
@@ -3,4 +3,12 @@ import ReactDOM from 'react-dom';
 
 import NavBar from './components/navbar.js'
 
+var fields = document.getElementsByTagName('input');
+for (let f of fields) {
+	f.onkeydown = function(e) {
+		if (e.keyCode == 13) // enter
+			search();
+	}
+}
+
 ReactDOM.render(<NavBar />, document.getElementsByTagName("nav")[0]);

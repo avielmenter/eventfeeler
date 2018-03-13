eae47bb6eaac076bcbc7dbb74c9c67d92f90397d
avielmenter <aviel.menter@gmail.com>
2018-03-12 20:43:25 -0700

    sentiment no longer shows for events with no comments

--- public/components/sentiment.js
+++ public/components/sentiment.js
@@ -12,6 +12,9 @@ export default class Sentiment extends React.Component {
 		if (this.props.neutral < 0.5)
 			classes = this.props.pos > 0.5 ? "happy glyphicon glyphicon-plus-sign" : "sad glyphicon glyphicon-minus-sign";
 
+		if (!this.props.pos || this.props.pos == 0)
+			classes = "";
+
 		return (
 			<span className={classes}></span>
 		);
